5e9b9eef78105532972c0e3ff4b18f53f41a5fc7
Omeiza17 <smichael15@alustudent.com>
2019-02-07 17:56:15 +0400

    3.4: Using Options Menus done!

--- app/src/main/java/com/pluralsight/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/pluralsight/notekeeper/NoteListActivity.java
@@ -4,17 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
-
-  private ArrayAdapter<NoteInfo> mAdapterNotes;
+  private NoteRecyclerAdapter mNoteRecyclerAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +35,15 @@ public class NoteListActivity extends AppCompatActivity {
   @Override
   protected void onResume() {
     super.onResume();
-    mAdapterNotes.notifyDataSetChanged();
+    mNoteRecyclerAdapter.notifyDataSetChanged();
   }
 
   private void initializeDisplayContent() {
-    final ListView listNotes = findViewById(R.id.list_notes);
-
+    final RecyclerView recyclerNotes = findViewById(R.id.list_notes);
+    final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
+    recyclerNotes.setLayoutManager(notesLayoutManager);
     List<NoteInfo> notes = DataManager.getInstance().getNotes();
-    mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
-
-    listNotes.setAdapter(mAdapterNotes);
-
-    listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-      @Override
-      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
-        intent.putExtra(NoteActivity.NOTE_POSITION, position);
-        startActivity(intent);
-      }
-    });
+    mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+    recyclerNotes.setAdapter(mNoteRecyclerAdapter);
   }
-
 }
